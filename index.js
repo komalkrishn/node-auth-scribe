83404728d4c7c96eb6e151ddb4e1c3a949d48058
komalkrishn <komal.kavula999@gmail.com>
2025-05-06 17:25:50 -0400

    Feat: add MVC for auth

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("auth");
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+// middleware to access public folder
+app.use(express.static('public'))
+
+// ejs view engine
+app.set('view engine', 'ejs')
+
+// database connection
+const dbURI = 'mongodb+srv://komalakavula416:PW5e83eXattkmD7f@cluster0.m4hsge3.mongodb.net/'
+mongoose.connect(dbURI)
+  .then((result) => {
+    console.log("connected")
+    app.listen(3000)
+  })
+  .catch((err) => console.log(err));
+
+  // routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
